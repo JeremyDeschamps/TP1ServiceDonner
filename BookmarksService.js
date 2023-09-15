615e6bf43f3b447a2fbb210aca8946b89291b6c0
JeremyDeschamps <Deschampsj007@gmail.com>
2023-09-15 13:50:35 -0400

    bookmarks

--- BookmarksService.js
+++ BookmarksService.js
@@ -25,24 +25,24 @@ function response(res, status, data = null) {
     res.end(data);
     return true;
 }
-async function handleContactsRequest(req, res) {
-    let contactsRepository = new Repository("./contacts.json");
-    let contact = null;
-    if (req.url == "/api/contacts") {
+async function handlebookmarksRequest(req, res) {
+    let bookmarksRepository = new Repository("./bookmarks.json");
+    let bookmark = null;
+    if (req.url == "/api/bookmarks") {
         switch (req.method) {
             case "GET":
-                return response(res, 200, JSON.stringify(contactsRepository.getAll()));
+                return response(res, 200, JSON.stringify(bookmarksRepository.getAll()));
             case "POST":
-                contact = await getPayload(req, res);
-                if (contact != null) {
-                    contact = contactsRepository.add(contact);
-                    return response(res, 201, JSON.stringify(contact));
+                bookmark = await getPayload(req, res);
+                if (bookmark != null) {
+                    bookmark = bookmarksRepository.add(bookmark);
+                    return response(res, 201, JSON.stringify(bookmark));
                 } else
                     return response(res, 400);
             case "PUT":
-                contact = await getPayload(req, res);
-                if (contact != null)
-                    if (contactsRepository.update(contact))
+                bookmark = await getPayload(req, res);
+                if (bookmark != null)
+                    if (bookmarksRepository.update(bookmark))
                         return response(res, 204);
                     else
                         return response(res, 404);
@@ -50,17 +50,17 @@ async function handleContactsRequest(req, res) {
                     return response(res, 400);
         }
     } else {
-        if (req.url.includes("/api/contacts/")) {
+        if (req.url.includes("/api/bookmarks/")) {
             let id = parseInt(req.url.substring(req.url.lastIndexOf("/") + 1, req.url.length));
             switch (req.method) {
                 case "GET":
-                    let contact = contactsRepository.get(id);
-                    if (contact !== null)
-                        return response(res, 200, JSON.stringify(contact));
+                    let bookmark = bookmarksRepository.get(id);
+                    if (bookmark !== null)
+                        return response(res, 200, JSON.stringify(bookmark));
                     else
                         return response(res, 404);
                 case "DELETE":
-                    if (contactsRepository.remove(id))
+                    if (bookmarksRepository.remove(id))
                         return response(res, 202);
                     else
                         return response(res, 404);
@@ -91,7 +91,7 @@ const server = createServer(async (req, res) => {
     console.log(req.method);
     accessControlConfig(req, res);
     if (!CORS_Preflight(req, res))
-        if (!handleContactsRequest(req, res))
+        if (!handlebookmarksRequest(req, res))
             response(res, 404);
 });
 const PORT = process.env.PORT || 5000;
